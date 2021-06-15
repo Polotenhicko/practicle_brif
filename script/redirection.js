1a2c7d697addb9de8e1f7f2b7ff7a6f8af2f187c
Polotenhicko <artem-gribanof@yandex.ru>
2021-06-15 13:59:11 +0300

    Сайт покупки -> сайт оплаты

--- script/redirection.js
+++ script/redirection.js
@@ -2,6 +2,6 @@ const $buttonProduct = document.querySelectorAll('.product_buy');
 
 for (let button of $buttonProduct) {
   button.onclick = function() {
-      alert('Перенаправление на сайт покупки!');  
+      alert('Перенаправление на сайт оплаты!');
   }
 }
